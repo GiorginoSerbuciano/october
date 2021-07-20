97dfe0dfbe4ab675c80abb7d4e292f3dd8c83d6a
GiorginoSerbuciano <64908978+GiorginoSerbuciano@users.noreply.github.com>
2021-07-20 16:55:00 +0200

    Testing scope

--- sketch.js
+++ sketch.js
@@ -1,53 +1,44 @@
 let shapeClassifier;
 let canvas;
-let img;
 let buttons;
-let label;
+
+let labels = [
+  "square",
+  "circle",
+  "triangle",
+]
+let label_pos = 0
+let current_label = labels[label_pos];
 let drawing_array = [];
 let drawing = [];
+let i = 0;
 
 function setup(){
-  createCanvas(windowWidth, windowHeight);
+  // canvas = createCanvas(windowWidth, windowHeight);
+  canvas = createCanvas(128,128);
+
   background(255);
-  console.log("RUNNING...");
+  console.log("RUNNING..."); 
 
   let options = {
-    inputs: [windowWidth, windowHeight, 4],
+    inputs: [128,128,4],
     task: "imageClassification",
     debug: "true"
   };
 
   shapeClassifier = ml5.neuralNetwork(options);
 
-  buttonsConfig();
-  
 }
 
-function buttonsConfig(){
 
-  buttons = {
-    "add": createButton('Add!'),
-    "label": createButton(label),
-    "train": createButton('Train!')
-  }
-  
-  buttons.add.position(50, 0);
-  buttons.train.position(0, 0);
-  buttons.label.position(94, 0);
-  
-  buttons.train.mousePressed();
-  buttons.add.mousePressed();
-  buttons.label.mousePressed();
-
+function finishedTraining(){
+  console.log(">>> TRAINING FINISHED! <<<")
+  // setTimeout(buttons.train = createButton("Train!"), 1000 )
 }
-
-// function windowResized() {
-//   resizeCanvas(windowWidth, windowHeight);
-// }
 
 function mousePressed() {
   // console.log("event: mousePressed()");
-
+  // x1 y1
   if (keyIsDown(SHIFT) === false){
     drawing = [];
   }
@@ -58,20 +49,28 @@ function keyPressed() {
   if (key === "c"){
     drawing = [];
     clear();
-    // BUG: Does not clear latest drawing!
-    // FIXED: draw() -> mouseDragged()
-    // BREAKS: SHIFT key line drawing!!
-
+    background(255);
+    // fix: SHIFT key line drawing!!
   } else if (key === "a"){
-    shapeClassifier.addData({ image: get(0, 0, 200, 200)})
+    img = canvas.get();
+    shapeClassifier.addData({image: img}, {label: current_label});
+    console.log(shapeClassifier.data);
 
+  } else if (key === "l"){
+    label_pos = label_pos++;
+    console.log(">>> LABEL:", current_label, "<<<");
+
+  } else if (key === "t"){
+    shapeClassifier.train({ epochs: 10 }, finishedTraining);
   }
 }
 
 function mouseReleased(){
   // console.log("event: mouseReleased()");
+  // x2 y2
   drawing_array.push(drawing);
-
+  // drawing = [];
+  // 
 }
 
 function mouseDragged(){
@@ -90,5 +89,4 @@ function mouseDragged(){
 
 function draw(){
 
- 
 }
