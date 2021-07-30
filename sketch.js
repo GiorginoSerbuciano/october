47bcf13e6f87964f32df691ee540c0415553cea5
GiorginoSerbuciano <serban.gorga@gmail.com>
2021-07-30 11:14:23 +0200

    Style conventions & removed label selection

--- sketch.js
+++ sketch.js
@@ -1,36 +1,19 @@
-// GLOBALS
 let shapeClassifier;  // ml5.neuralnetwork()
 let canvas;  // createCanvas()
-
-// LABELS AND CYCLING
-let labels = ["square","circle","triangle"];  
-let i = 0;  // MOVE INTO LOCAL
-let current_label = labels[0];  // defaults drawing label to square
-
-// CONTAINER FOR DRAWINGS
-let DRAW = {
-  FREE:[],  // vertices of current drawing
-  LINE:{  // coords of current line
-    x1:undefined,
-    y1:undefined,
-    x2:undefined,
-    y2:undefined,
-  },
-  free_store:[],
-  line_store:[]
-}
-
+const canvasSize = {
+  height:255, 
+  width:255, 
+  color:255
+};
 
 function setup(){
-  const canvas_size = {height:255, width:255, color:255};
-  canvas = createCanvas(canvas_size.width, canvas_size.height);
+  canvas = createCanvas(canvasSize.width, canvasSize.height);
 
-  background(canvas_size.color);
+  background(canvasSize.color);
   console.log("RUNNING..."); 
-  console.log(">>> LABEL:", current_label, "<<<");
 
   let options = {
-    inputs: [canvas_size.width, canvas_size.height,  4],
+    inputs: [canvasSize.width, canvasSize.height,  4],
     task: "imageClassification",
     debug: "true"
   };
@@ -43,65 +26,52 @@ function setup(){
 function buttonsConfig(){
 
   buttons = {
-    "add": createButton('Add!'),
-    "label": createButton('Click me to change label!'),
     "train": createButton('Train!'),
-    "auto_train": createButton('Auto train!')
-    // Added Button to start automatic training 
+    "generate": createButton('Generate!')
   }
   
-  buttons.add.position(50, 270);
-  buttons.train.position(0, 270);
-  buttons.label.position(94, 270);
-  buttons.auto_train.position(0, 290);
-  //Moved all the buttons below the canvas
+  buttons.train.position(0, canvasSize.height + 10);
+  buttons.generate.position(50, canvasSize.height + 10);
   
   buttons.train.mousePressed(startTraining);
-  buttons.add.mousePressed(addShape);
-  buttons.label.mousePressed(changeLabel);
-  buttons.auto_train.mousePressed(createDataSet);
+  buttons.generate.mousePressed(createDataSet);
 
 }
 
+
+let trainingLabels = ["square","circle","triangle"];  
 //function to create the training data for the NN
 function createDataSet() {
     //create data to recognize shapes 
     //make a "set" varibale for how many of each shape are created
+    let setSize = 10;
     for ( shape = 0; shape < 3; shape++){
-      r = random (10, canvas_size.height/3);
-      x = random(r, canvas_size.width - r);
-      y = random(r, canvas_size.height - r);
+      r = random (10, canvasSize.height/3);
+      x = random(r, canvasSize.width - r);
+      y = random(r, canvasSize.height - r);
       translate(x, y);
       if (shape == 0) {    //create data to recognize squares
-        for ( i = 0; i < 100; i++) {
+        for ( i = 0; i < setSize; i++) {
           clearCanvas();
           rectMode(CENTER);
           rotate(random(-0.1, 0.1));
           square(0, 0, r*2);
-          label = labels[0];
-          addShape();
+          addShape(trainingLabels[0]);
       }
-      console.log(label);
     } else if (shape == 1) {       //create data to recognize circles
-        for ( i = 0; i < 100; i++) {
+        for ( i = 0; i < setSize; i++) {
           clearCanvas();
           circle(x, y, r*2);
-          label = labels[1];
-          addShape();
+          addShape(trainingLabels[1]);
         }
-        console.log(label);
       } else if (shape == 2) {     //create data to recognize triangles
-          for ( i = 0; i < 100; i++) {
+          for ( i = 0; i < setSize; i++) {
            clearCanvas();
            rotate(random(-0.1, 0.1));
            triangle(0, -r, r, r, -r, r);
-           label = labels[2];
-           addShape();
+           addShape(trainingLabels[2]);
       }
-      console.log(label);
     }
-  //run the training?
-  //startTraining();
   }
 }
 
@@ -111,44 +81,30 @@ function finishedTraining(){  // callback for shapeClassifier.train()
 }
 
 
-function clearCanvas() {
-  drawing = [];
-  clear();
-  background(canvas_size.color);
+
+function addShape(label){
+  img = canvas.get();
+  shapeClassifier.addData({image: img}, {label: label});
+  console.log ("Added a", label, "to the training dataset!")
 }
 
-function keyPressed() {
 
-  if (key === "c"){  // clears the canvas
-    DRAW = {
-      FREE:[],
-      LINE:{
-        x1:undefined,
-        y1:undefined,
-        x2:undefined,
-        y2:undefined,
-      },
-      free_store:[],
-      line_store:[]
-    }
-    clear();
-    background(canvas_size.color);
-
-  } else if (key === "a"){  // adds the canvas i.e. the drawn shape to the NN's data
-    img = canvas.get();
-    shapeClassifier.addData({image: img}, {label: current_label});
-
-  } else if (key === "l"){  // goes to the next label
-    changeLabel(); 
-    console.log(">>> LABEL:", current_label, "<<<");
-
-
-  } else if (key === "t"){  // begins training
-    shapeClassifier.train({ epochs: 50 }, finishedTraining);
-  }
-
+function startTraining(epochs){
+  shapeClassifier.train({ epochs: epochs }, finishedTraining);
 }
 
+
+let DRAW = {
+  FREE:[],
+  LINE:{
+    x1:undefined,
+    y1:undefined,
+    x2:undefined,
+    y2:undefined,
+  },
+  freeStore:[],
+  lineStore:[]
+}
 // DRAWING CONTROLS START HERE 
 function mousePressed() {
 
@@ -174,7 +130,7 @@ function mouseDragged(){
     let y1 = DRAW.LINE.y1;
     let x2 = DRAW.LINE.x2;
     let y2 = DRAW.LINE.y2;
-    background(canvas_size.color);  // ALLOWS A SINGLE 'PREVIEW' LINE
+    background(canvasSize.color);  // ALLOWS A SINGLE 'PREVIEW' LINE
     line(x1,y1,x2,y2);  
   }
 
@@ -182,21 +138,38 @@ function mouseDragged(){
 
 function mouseReleased() {
 
-  DRAW.free_store.push(DRAW.FREE);
-  DRAW.line_store.push(DRAW.LINE);
+  DRAW.freeStore.push(DRAW.FREE);
+  DRAW.lineStore.push(DRAW.LINE);
 
 }
 
 
-// FRAME UPDATES
 function drawLines(){  // RE-DRAWS ALL STORED LINES
 
-  let l = DRAW.line_store;
+  let l = DRAW.lineStore;
   for (let i = 0; i < l.length; i++){  
     line(l[i].x1,l[i].y1,l[i].x2,l[i].y2);
   }
 
 }
+
+
+function clearCanvas() {
+  DRAW = {
+    FREE:[],
+    LINE:{
+      x1:undefined,
+      y1:undefined,
+      x2:undefined,
+      y2:undefined,
+    },
+    freeStore:[],
+    lineStore:[]
+  }
+  clear();
+  background(canvasSize.color);
+}
+
 
 function drawSketches(array){  // RE-DRAWS ALL STORED FREEHAND DRAWINGS
 
@@ -213,8 +186,8 @@ function drawSketches(array){  // RE-DRAWS ALL STORED FREEHAND DRAWINGS
 function draw() {
   
   drawLines();
-  for (let i = 0; i < DRAW.free_store.length; i++){
-    drawSketches(DRAW.free_store[i]);
+  for (let i = 0; i < DRAW.freeStore.length; i++){
+    drawSketches(DRAW.freeStore[i]);
   }
   
 }
