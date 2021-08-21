18a1303cbadad5327b8b9582592feb594eaae169
GiorginoSerbuciano <serban.gorga@gmail.com>
2021-08-21 10:44:01 +0200

    NN init in sketch.js & drawing adjustments - Increased stroke weight

--- sketch.js
+++ sketch.js
@@ -1,9 +1,33 @@
 let canvas;  // createCanvas()
-const CANVASPROPERTIES = {
-  height:255, 
-  width:255, 
-  color:255,
-};
+let bkg = 255;
+let weight = 2;
+
+function setup(){
+  canvas = createCanvas(512,512);
+  background(bkg);
+  let options = {
+    inputs: [64, 64, 4],
+    task: "imageClassification",
+    debug: "true",
+  }
+  shapeClassifier = ml5.neuralNetwork(options);
+  for (label in shapeCollection){
+    for (item in shapeCollection[label]){
+    let image = shapeCollection[label][item];
+    shapeClassifier.addData( {image: image}, {label: label} );
+    }
+  }
+  shapeClassifier.normalizeData(); 
+  const modelDetails = {
+    model: 'model/model.json',
+    metadata: 'model/model_meta.json',
+    weights: 'model/model.weights.bin'
+  }
+  shapeClassifier.load(modelDetails, modelLoaded);
+  buttonsConfig();
+}
+
+
 let drawCollection = {
   freeDrawing:[],
   lineDrawing: {
@@ -15,24 +39,6 @@ let drawCollection = {
   freeStore:[],
   lineStore:[],
 }
-
-function setup(){
-  canvas = createCanvas(CANVASPROPERTIES.width, CANVASPROPERTIES.height);
-
-  background(CANVASPROPERTIES.color);
-  console.log("RUNNING..."); 
-
-  let options = {
-    inputs: [128, 128, 4],
-    task: "imageClassification",
-    debug: "true"
-  };
-
-  shapeClassifier = ml5.neuralNetwork(options);
-  buttonsConfig();
-}
-
-
 function clearCanvas() {
   drawCollection = {
     freeDrawing:[],
@@ -46,7 +52,7 @@ function clearCanvas() {
     lineStore:[],
   }
   clear();
-  background(CANVASPROPERTIES.color);
+  background(bkg);
 }
 
 
@@ -63,6 +69,7 @@ function mouseDragged() {
   if (keyIsDown(SHIFT) === false) {  // FREEHAND DRAWING CONTROLS
     drawCollection.freeDrawing.push( [mouseX,mouseY] ); // pushing every x/y coordinate of the pressed mouse
     noFill();
+    strokeWeight(weight);
     drawSketches(drawCollection.freeDrawing);
   } else if (keyIsDown(SHIFT) === true) {  // LINE DRAWING CONTROLS
     let l = drawCollection.lineDrawing;
@@ -71,7 +78,7 @@ function mouseDragged() {
     let y1 = drawCollection.lineDrawing.y1;
     let x2 = drawCollection.lineDrawing.x2;
     let y2 = drawCollection.lineDrawing.y2;
-    background(CANVASPROPERTIES.color);  // ALLOWS A SINGLE 'PREVIEW' LINE
+    background(bkg);  // ALLOWS A SINGLE 'PREVIEW' LINE
     line(x1, y1, x2, y2);  
   }
 }
@@ -85,6 +92,8 @@ function mouseReleased() {
 
 function drawLines() {  // RE-DRAWS ALL STORED LINES
   let l = drawCollection.lineStore;
+  strokeWeight(weight);
+
   for (let i = 0; i < l.length; i++) {  
     line(l[i].x1, l[i].y1, l[i].x2, l[i].y2);
   }
@@ -93,6 +102,8 @@ function drawLines() {  // RE-DRAWS ALL STORED LINES
 
 function drawSketches(array) {  // RE-DRAWS ALL STORED FREEHAND DRAWINGS
   beginShape();
+  strokeWeight(weight);
+
   for (let i = 0; i < array.length; i++) { 
     let x = array[i][0];
     let y = array[i][1];
@@ -101,7 +112,9 @@ function drawSketches(array) {  // RE-DRAWS ALL STORED FREEHAND DRAWINGS
   endShape(); 
 }
 
+
 function draw() {
+  strokeWeight(weight);
   drawLines();
   for (let i = 0; i < drawCollection.freeStore.length; i++) {
     drawSketches(drawCollection.freeStore[i]);
